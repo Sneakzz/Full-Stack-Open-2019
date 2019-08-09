4c9676174cc341e415f8058f84ef2ede3bdf4086
Sneakzz <kenny.veys@gmail.com>
2019-08-09 04:21:33 +0200

    Modify code as introduction to basic re-rendering of components

--- part1/lesson/src/index.js
+++ part1/lesson/src/index.js
@@ -1,40 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by
-      <a href="https://github.com/Sneakzz">Kenny</a>
-    </div>
-  )
-}
-
 // This is a React Component called "App"
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
+const App = (props) => {
+  const {counter} = props
   return (
-    // use the empty tags to avoid having "extra" div's in the DOM tree
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Footer />
-    </>
+    <div>{counter}</div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />,
+    document.getElementById('root'))
+}
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
+
+ReactDOM.render(
+  <App counter={counter} />,
+  document.getElementById('root')
+)
