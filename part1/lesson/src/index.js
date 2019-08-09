82cd6c2490d8e5b20f299bb0fbd36e96c46720aa
Sneakzz <kenny.veys@gmail.com>
2019-08-09 04:14:37 +0200

    Modify code by destructuring it

--- part1/lesson/src/index.js
+++ part1/lesson/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
@@ -12,7 +15,7 @@ const Hello = (props) => {
 const Footer = () => {
   return (
     <div>
-      greeting app created by 
+      greeting app created by
       <a href="https://github.com/Sneakzz">Kenny</a>
     </div>
   )
