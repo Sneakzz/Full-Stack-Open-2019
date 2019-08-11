6fe46aa4da5d0dcefc7f15f2fb5868690eded2e2
Sneakzz <kenny.veys@gmail.com>
2019-08-11 21:38:44 +0200

    Modify code to an example of passing state down to child components

--- part1/lesson/src/index.js
+++ part1/lesson/src/index.js
@@ -4,21 +4,34 @@ import ReactDOM from 'react-dom';
 // This is a React Component called "App"
 const App = (props) => {
   const [ counter, setCounter ] = useState(0)
-
-  const setToValue = (value) => () => setCounter(value)
+  const setToValue = (value) => setCounter(value)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={setToValue(counter + 1)}>
-        plus
-      </button>
-      <button onClick={setToValue(0)}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button
+        onClick={() => setToValue(counter + 1)}
+        text='plus'
+      />
+      <Button
+        onClick={() => setToValue(counter - 1)}
+        text='minus'
+      />
+      <Button
+        onClick={() => setToValue(0)}
+        text='zero'
+      />
     </div>
   )
 }
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 ReactDOM.render(
   <App />,
