2288751bb1d14270c6eb628b7db97989f24e0c4e
Sneakzz <kenny.veys@gmail.com>
2019-08-12 21:38:35 +0200

    Add code for simple rating system - unicafe step 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Header = ({text}) => (
+  <div>
+    <h1>{text}</h1>
+  </div>
+)
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Outcome = ({text, value}) => (
+  <div>
+    {text} {value}
+  </div>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodBtn = () => setGood(good + 1)
+  const handleNeutralBtn = () => setNeutral(neutral + 1)
+  const handleBadBtn = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Header text="give feedback" />
+      <Button onClick={handleGoodBtn} text="good" />
+      <Button onClick={handleNeutralBtn} text="neutral" />
+      <Button onClick={handleBadBtn} text="bad" />
+
+      <Header text="statistics" />
+      <Outcome text="good" value={good} />
+      <Outcome text="neutral" value={neutral} />
+      <Outcome text="bad" value={bad} />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
