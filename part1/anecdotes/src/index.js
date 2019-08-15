7ae212db879b7c663eaaf29d0d449a100f43b19e
Sneakzz <kenny.veys@gmail.com>
2019-08-15 17:06:14 +0200

    End of anecdotes step 3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,42 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = ({text}) => (
+  <div>
+    <h1>{text}</h1>
+  </div>
+)
+
+const Anecdote = ({text, votes}) => (
+  <div>
+    <div>
+      {text}
+    </div>
+    <div>
+      has {votes} votes
+    </div>
+  </div>
+)
+
+const TopAnecdote = ({anecdotes, votes}) => {
+  let topVotedIndex = votes.indexOf(Math.max(...votes))
+
+  return (
+    <div>
+      <div>
+        {anecdotes[topVotedIndex]}
+      </div>
+      <div>
+        has {votes[topVotedIndex]} votes
+      </div>
+    </div>
+  )
+}
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>{text}</button>
+)
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
@@ -23,16 +59,13 @@ const App = (props) => {
 
   return (
     <div>
-      <div>
-        {props.anecdotes[selected]}
-      </div>
-      <div>
-        has {votes[selected]} votes
-      </div>
-      <div>
-        <button onClick={handleVoteBtn}>vote</button>
-        <button onClick={handleNextBtn}>next anecdote</button>
-      </div>
+      <Header text="Anecdote of the day" />
+      <Anecdote text={props.anecdotes[selected]} votes={votes[selected]} />
+      <Button onClick={handleVoteBtn} text="vote" />
+      <Button onClick={handleNextBtn} text="next anecdote" />
+
+      <Header text="Anecdote with most votes" />
+      <TopAnecdote anecdotes={props.anecdotes} votes={votes} />
     </div>
   )
 }
