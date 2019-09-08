8d19a32562cfc1937a490e32b90af52bc1eb82bd
Sneakzz <kenny.veys@gmail.com>
2019-09-08 23:22:32 +0200

    End of part2 step9

--- part2/course-contents/src/index.js
+++ part2/course-contents/src/index.js
@@ -2,14 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// const Total = (props) => {
-//   const result = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
-
-//   return (
-//     <div>
-//       <p>number of exercises {result}</p>
-//     </div>
-//   )
-// }
-
 ReactDOM.render(<App />, document.getElementById('root'));
