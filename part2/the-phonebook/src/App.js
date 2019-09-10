449d278d488d4bba4d9f2ad107041ae09657176f
Sneakzz <kenny.veys@gmail.com>
2019-09-10 22:29:19 +0200

    End of step4

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -5,22 +5,35 @@ const App = () => {
     {
       name: 'Arto Hellas',
       number: '040-1234567'
+    },
+    {
+      name: 'Ada Lovelace',
+      number: '39-44-5323523'
+    },
+    {
+      name: 'Dan Abramov',
+      number: '12-43-234345'
+    },
+    {
+      name: 'Mary Poppendieck',
+      number: '39-23-6423122'
     }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const addPerson = event => {
     event.preventDefault();
 
     if (persons.some(person => person.name === newName)) {
       return alert(`${newName} is already added to phonebook`);
-    }
+    };
 
     const personObj = {
       name: newName,
       number: newNumber
-    }
+    };
 
     setPersons(persons.concat(personObj));
     setNewName('');
@@ -29,21 +42,38 @@ const App = () => {
 
   const newNameChange = event => {
     setNewName(event.target.value);
-  }
+  };
 
   const newNumberChange = event => {
     setNewNumber(event.target.value);
-  }
+  };
 
-  const renderPersons = () => persons.map(person =>
-    <div key={person.name}>
-      {person.name} {person.number}
-    </div>
-  )
+  const filterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const renderPersons = () => {
+    const filteredPersons = persons.filter(person => {
+
+      return person.name.toLowerCase().startsWith(filter.toLowerCase());
+    });
+
+    console.log(filteredPersons);
+
+    return (filteredPersons.map(person =>
+      <div key={person.name}>
+        {person.name} {person.number}
+      </div>
+    ));
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={filterChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={newNameChange} />
