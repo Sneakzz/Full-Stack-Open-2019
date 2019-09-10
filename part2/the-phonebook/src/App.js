eb6189993247161d3c42fcee508a55418492e37a
Sneakzz <kenny.veys@gmail.com>
2019-09-10 03:00:31 +0200

    End of step 3

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
-  const addName = event => {
+  const addPerson = event => {
     event.preventDefault();
 
     if (persons.some(person => person.name === newName)) {
@@ -14,29 +18,38 @@ const App = () => {
     }
 
     const personObj = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     setPersons(persons.concat(personObj));
     setNewName('');
+    setNewNumber('');
   };
 
   const newNameChange = event => {
     setNewName(event.target.value);
   }
 
+  const newNumberChange = event => {
+    setNewNumber(event.target.value);
+  }
+
   const renderPersons = () => persons.map(person =>
     <div key={person.name}>
-      {person.name}
+      {person.name} {person.number}
     </div>
   )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={newNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={newNumberChange} />
         </div>
         <div>
           <button type='submit'>add</button>
