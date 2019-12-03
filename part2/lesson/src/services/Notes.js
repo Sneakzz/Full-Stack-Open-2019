9036a3f312bf9e2c80651a79f2bf2abf21c2c344
Sneakzz <kenny.veys@gmail.com>
2019-12-03 22:37:04 +0100

    Change backend url from local to remote

--- part2/lesson/src/services/Notes.js
+++ part2/lesson/src/services/Notes.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/notes';
+const baseUrl = 'https://radiant-tor-09871.herokuapp.com/notes';
+//const baseUrl = 'http://localhost:3001/notes';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
