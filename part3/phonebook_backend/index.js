abe53eb537812346a45be0cacc520dd426c6307b
Sneakzz <kenny.veys@gmail.com>
2019-12-05 01:32:35 +0100

    phonebook backend step 9 - make changes so backend works with frontend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(morgan('tiny', {
