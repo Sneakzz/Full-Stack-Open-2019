d94566997c865a5b4f5c7b0e779cb9ac6c67b211
Sneakzz <kenny.veys@gmail.com>
2019-12-09 23:58:15 +0100

    add error handling + set up/fix remaining routes

--- part3/lesson/index.js
+++ part3/lesson/index.js
@@ -5,19 +5,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const Note = require('./models/note');
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static('build'));
-
-const requestLogger = (request, response, next) => {
-  console.log('Method: ', request.method);
-  console.log('Path: ', request.path);
-  console.log('Body: ', request.body);
-  console.log('---');
-  next();
-}
-app.use(requestLogger);
-
 let notes = [
   {
     id: 1,
@@ -39,6 +26,19 @@ let notes = [
   }
 ]
 
+const requestLogger = (request, response, next) => {
+  console.log('Method: ', request.method);
+  console.log('Path: ', request.path);
+  console.log('Body: ', request.body);
+  console.log('---');
+  next();
+}
+
+app.use(express.static('build'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(requestLogger);
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
@@ -53,14 +53,16 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
     .then(note => {
-      res.json(note.toJSON());
+      if(note){
+        res.json(note.toJSON());
+      } else {
+        res.status(404).end();
+      }
     })
-    .catch(err => {
-      console.log('something went wrong trying to get a note');
-    });
+    .catch(err => next(err));
 });
 
 const generateId = () => {
@@ -94,11 +96,27 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter(note => note.id !== id);
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body;
 
-  res.status(204).end();
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote.toJSON());
+    })
+    .catch(err => next(err));
+});
+
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(err => next(err));
 });
 
 const unknownEndpoint = (req, res) => {
@@ -106,6 +124,18 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message);
+
+  if(err.name === 'CastError' && err.kind === 'ObjectId'){
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(err);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
